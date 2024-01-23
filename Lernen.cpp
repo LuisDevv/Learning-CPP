33bf478abd4612e473f4cb8a93b7b2c39aefe461
Penny <80627655+LuisDevv@users.noreply.github.com>
2024-01-23 23:04:55 +0100

    Update Lernen.cpp
    first turtorial learned

--- Lernen.cpp
+++ Lernen.cpp
@@ -1,1 +1,21 @@
-t
+// imports
+#include <iostream>
+
+// "importing" std instead of std::cout
+using namespace std;
+
+
+// main class
+int main() {
+	// printing Hello World and goes to next line using endl in same funciton
+	cout << "Hello World!" << endl;
+
+	// goes one line down same as \n
+	// cout << endl;
+
+	// press any key to countinue
+	system("pause");
+
+	// end program with 0
+	return 0;
+}
